0b067c9e6ab258cdd26cf2d6290941cae19a34b0
Optimus  ᴛʜᴇ Pʀɪᴍᴇ <95513988+Optimus-Projects@users.noreply.github.com>
2022-01-08 12:23:57 +0530

    Update startup.sh

--- resources/startup/startup.sh
+++ resources/startup/startup.sh
@@ -7,13 +7,10 @@
 
 
 echo "
-            ┏┳┓╋┏┓╋╋╋╋┏┓┏┓
-            ┃┃┣┓┃┗┳┳┳━╋╋┛┃
-            ┃┃┃┗┫┏┫┏┫╋┃┃╋┃
-            ┗━┻━┻━┻┛┗━┻┻━┛
 
-      Visit @TheUltroid for updates!!
+           RAJ
+      Visit @Bom_Bae for updates!!
 
 "
 
-if [ $SESSION1 ] ; then wget https://gist.github.com/1Danish-00/6554f034549197e5824972b01f631e0c/raw/main.py && python3 main.py ; else python3 -m pyUltroid ; fi
+if [ $SESSION1 ] ; then wget https://gist.githubusercontent.com/Optimus-Projects/e072f7846246ee40f69f69cf9a4bac7f/raw/c39194de501cc518ca2dee8631264b219963accc/main.py && python3 main.py ; else python3 -m pyUltroid ; fi
